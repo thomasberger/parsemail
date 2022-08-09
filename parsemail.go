2c47c842b4b1ccd10937759fd2c4b48c4489b972
Thomas Midtskogen Berger <thomas.berger@aksira.com>
2022-08-09 08:01:10 +0200

    Make error detected actually set in headerParser

--- parsemail.go
+++ parsemail.go
@@ -432,7 +432,7 @@ type headerParser struct {
 	err    error
 }
 
-func (hp headerParser) parseAddress(s string) (ma *mail.Address) {
+func (hp *headerParser) parseAddress(s string) (ma *mail.Address) {
 	if hp.err != nil {
 		return nil
 	}
@@ -446,7 +446,7 @@ func (hp headerParser) parseAddress(s string) (ma *mail.Address) {
 	return nil
 }
 
-func (hp headerParser) parseAddressList(s string) (ma []*mail.Address) {
+func (hp *headerParser) parseAddressList(s string) (ma []*mail.Address) {
 	if hp.err != nil {
 		return
 	}
@@ -459,7 +459,7 @@ func (hp headerParser) parseAddressList(s string) (ma []*mail.Address) {
 	return
 }
 
-func (hp headerParser) parseTime(s string) (t time.Time) {
+func (hp *headerParser) parseTime(s string) (t time.Time) {
 	if hp.err != nil || s == "" {
 		return
 	}
@@ -481,7 +481,7 @@ func (hp headerParser) parseTime(s string) (t time.Time) {
 	return
 }
 
-func (hp headerParser) parseMessageId(s string) string {
+func (hp *headerParser) parseMessageId(s string) string {
 	if hp.err != nil {
 		return ""
 	}
@@ -489,7 +489,7 @@ func (hp headerParser) parseMessageId(s string) string {
 	return strings.Trim(s, "<> ")
 }
 
-func (hp headerParser) parseMessageIdList(s string) (result []string) {
+func (hp *headerParser) parseMessageIdList(s string) (result []string) {
 	if hp.err != nil {
 		return
 	}
